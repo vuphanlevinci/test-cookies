9b2e1a11343632a223e9b48e367f17635a9b7fef
Vu Phan <vu.pham@risewithaurora.com>
2024-05-13 14:49:58 +0700

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <iframe
+          style={{ width: "90vw", height: "90vh" }}
+          src="https://staging-giftcard.nailsoft.com/?merchant_id=681&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZXJjaGFudElkIjoiNjgxIiwiZGlnaXRhbFR5cGUiOiJnaWZ0Y2FyZF9wbHVnaW4iLCJyb2xlIjoiR2lmdENhcmRXZWJzaXRlIiwibmJmIjoxNzE0MTA1MTE2LCJleHAiOjIwMjk2Mzc5MTYsImlhdCI6MTcxNDEwNTExNiwiaXNzIjoiaHR0cHM6Ly9zdGFnaW5nLWFwaS5uYWlsc29mdC5jb20vYXBpLyIsImF1ZCI6Ik5TX0FQSV9DbGllbnQifQ.dVGphB9tlkodQhO8TSA-Z65LzfZY6DTCmBcc1Ddvhgo"
+        ></iframe>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
