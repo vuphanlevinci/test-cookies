76aa385f90229426d8049248f29903da86995549
Vu Phan <vu.pham@risewithaurora.com>
2024-06-28 09:31:01 +0700

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ function App() {
       <div className="container">
         <iframe
           style={{ width: "90vw", height: "90vh" }}
-          // allowFullScreen
+          allowFullScreen
           // loading="lazy"
-          // referrerPolicy="no-referrer-when-downgrade"
+          referrerPolicy="no-referrer-when-downgrade"
           src="https://60fb-123-21-195-95.ngrok-free.app?merchant_id=1000&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZXJjaGFudElkIjoiMTAwMCIsImRpZ2l0YWxUeXBlIjoiYm9va2luZ19wbHVnaW4iLCJyb2xlIjoiRGlnaXRhbFdlYnNpdGUiLCJuYmYiOjE3MDY3NzI5MTIsImV4cCI6MjAyMjM5MjExMiwiaWF0IjoxNzA2NzcyOTEyLCJpc3MiOiJodHRwczovL3N0YWdpbmctYXBpLm5haWxzb2Z0LmNvbS9hcGkvIiwiYXVkIjoiTlNfQVBJX0NsaWVudCJ9.ZTYxBSwgMI3eDX-Y_TmUA3eH6kVz5hz8tjw9QmjOwAk"
         ></iframe>
       </div>
